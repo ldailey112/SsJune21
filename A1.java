89e50bb319f2b35a374d4ea9b85a200d2371066d
ldailey112 <85193874+ldailey112@users.noreply.github.com>
2021-06-01 23:19:55 -0500

    Add files via upload

---
+++ A1.java
@@ -0,0 +1,43 @@
+public class A1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		//First set, left triangle
+		System.out.println("1)");
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		//System.out.println("");
+		System.out.println(".........");
+		
+		//Second set inverted
+		System.out.println("2)");
+		System.out.println("..........");
+		System.out.println("****");
+		System.out.println("***");
+		System.out.println("**");
+		System.out.println("*");
+		
+		//Third set pyramid
+		System.out.println("3)");
+		System.out.println("     *");
+		System.out.println("    ***");
+		System.out.println("   *****");
+		System.out.println("  *******");
+		//System.out.println("");
+		System.out.println("...........");
+		
+		//Fourth set inverted
+		System.out.println("4)");
+		System.out.println("............");
+		System.out.println("  *******");
+		System.out.println("   *****");
+		System.out.println("    ***");
+		System.out.println("     *");
+
+	}
+
+}
